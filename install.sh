a64724c98590a5f530e638839afab6de9c4225d0
brucewayne <you@example.com>
2020-06-18 17:36:06 +0100

    added packages for unmanaged powershell

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 # install stuff we need
 apt-get update -y
-apt-get install -y python3.8 python3-pip python3-openssl gcc-mingw-w64 python3.8-dev nasm upx binutils wget apt-transport-https procps
+apt-get install -y python3.8 python3-pip python3-openssl gcc-mingw-w64 python3.8-dev nasm upx binutils wget apt-transport-https procps mono-complete less
 python3.8 -m pip install -r /root/shad0w/requirements.txt
 
 # install dotnet
