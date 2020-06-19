6abca10d697d2b9332844d9a896a7886de421f2b
batsec <63263302+bats3c@users.noreply.github.com>
2020-06-19 10:03:18 +0100

    moved metasploit to same line as other packages

--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 # install stuff we need
 apt-get update -y
-apt-get install -y python3.8 python3-pip python3-openssl gcc-mingw-w64 python3.8-dev nasm upx binutils wget apt-transport-https procps mono-complete less
+apt-get install -y python3.8 python3-pip python3-openssl gcc-mingw-w64 python3.8-dev nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
 python3.8 -m pip install -r /root/shad0w/requirements.txt
-apt install -y metasploit-framework
 
 # install dotnet
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
