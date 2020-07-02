a3f20496c3199ccd656846055484a04eeb283424
brucewayne <you@example.com>
2020-07-02 01:23:31 +0100

    added injectable beacon build

---
+++ beacon/injectable/debug.c
@@ -0,0 +1,16 @@
+#include <stdarg.h>
+
+void DEBUG(const char* text, ...)
+{
+    va_list ap;
+    char* string;
+
+    va_start(ap, text);
+    if(0 > vasprintf(&string, text, ap))
+    {
+        string = "FAILED TO FORMAT";
+    }
+    va_end(ap);
+
+    printf("[DEBUG] %s\n", string); 
+}
