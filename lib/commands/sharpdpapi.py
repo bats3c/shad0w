05ee0c5512f4494e9ba42d3319b245a56c634eb9
Lee Baird <leebaird@gmail.com>
2021-01-05 15:39:39 -0600

    Changed /root/ to /opt/ and removed blank line.

--- lib/commands/sharpdpapi.py
+++ lib/commands/sharpdpapi.py
@@ -3,7 +3,6 @@
 #
 
 import argparse
-
 from lib import shellcode
 
 __description__ = "A C# port of some DPAPI functionality from mimikatz"
@@ -13,7 +12,7 @@ __author__ = "@_batsec_, @harmj0y"
 USERCD_EXEC_ID = 0x3000
 
 # location of sharpdpapi binary
-SHARPDPAPI_BIN = "/root/shad0w/bin/SharpCollection/NetFramework_4.5_x86/SharpDPAPI.exe"
+SHARPDPAPI_BIN = "/opt/shad0w/bin/SharpCollection/NetFramework_4.5_x86/SharpDPAPI.exe"
 
 # little hack but lets us pass the args to donut
 class DummyClass(object):
