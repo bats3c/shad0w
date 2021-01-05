2d7f6195e3683ad9cf5f984c955c60bb79212388
Lee Baird <leebaird@gmail.com>
2021-01-05 15:37:45 -0600

    Changed /root/ to /opt/ and removed blank line.

--- lib/commands/seatbelt.py
+++ lib/commands/seatbelt.py
@@ -3,7 +3,6 @@
 #
 
 import argparse
-
 from lib import shellcode
 
 __description__ = "A project that performs a number of security oriented host-survey \"safety checks\""
@@ -13,7 +12,7 @@ __author__ = "@_batsec_, @harmj0y"
 USERCD_EXEC_ID = 0x3000
 
 # location of seatbelt binary
-SEATBELT_BIN = "/root/shad0w/bin/SharpCollection/NetFramework_4.5_x86/Seatbelt.exe"
+SEATBELT_BIN = "/opt/shad0w/bin/SharpCollection/NetFramework_4.5_x86/Seatbelt.exe"
 
 # little hack but lets us pass the args to donut
 class DummyClass(object):
