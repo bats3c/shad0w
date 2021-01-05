b3996ff18fae9812f97ecf8fa6979f914d364bef
Lee Baird <leebaird@gmail.com>
2021-01-05 15:36:48 -0600

    Changed /root/ to /opt/. and imports in order.

--- lib/commands/safetykatz.py
+++ lib/commands/safetykatz.py
@@ -3,7 +3,6 @@
 #
 
 import argparse
-
 from lib import shellcode
 
 __description__ = "A tool to minidump lsass then use mimikatz sekurlsa::logonpasswords & sekurlsa::ekeys on it"
@@ -13,7 +12,7 @@ __author__ = "@_batsec_, @harmj0y"
 USERCD_EXEC_ID = 0x3000
 
 # location of safetykatz binary
-SAFETYKATZ_BIN = "/root/shad0w/bin/SharpCollection/NetFramework_4.5_x86/SafetyKatz.exe"
+SAFETYKATZ_BIN = "/opt/shad0w/bin/SharpCollection/NetFramework_4.5_x86/SafetyKatz.exe"
 
 # little hack but lets us pass the args to donut
 class DummyClass(object):
