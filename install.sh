336912d9a4514bb3c567d6c7b134b2d7f1d3b0f1
batsec <63263302+bats3c@users.noreply.github.com>
2021-01-20 10:05:58 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # install stuff we need
 apt-get update -y
-apt-get install -y python3.9 python3-pip python3-openssl gcc-mingw-w64 nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
-python3.9 -m pip install -r /root/shad0w/requirements.txt
+apt-get install -y python3 python3-pip python3-openssl gcc-mingw-w64 nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
+python3 -m pip install -r /root/shad0w/requirements.txt
 
 # install dotnet
 wget https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb -O /tmp/packages-microsoft-prod.deb
